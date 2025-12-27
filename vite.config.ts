77af211e940094d6fa4663230f4f955907bc7aa7
jgfrigo2 <josep.g.frigole2@gmail.com>
2025-12-27 22:19:50 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
+    base: '/garco-comics/', // <-- required for GitHub Pages
+    plugins: [react()],
     const env = loadEnv(mode, '.', '');
     return {
       server: {
